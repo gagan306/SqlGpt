e697b78c3153ff6e06ff3361f550ff0864c82bf0
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-12 22:16:03 +0545

    still doing

--- src/api/RenderChat.js
+++ src/api/RenderChat.js
@@ -1,13 +1,13 @@
-// src/api/api.js
+
 
 export async function fetchMessages() {
     try {
-      const response = await fetch('http://localhost:5000/api/messages'); // your backend URL
+      const response = await fetch('http://localhost:5000/api/chat'); 
       if (!response.ok) {
         throw new Error('Failed to fetch messages');
       }
       const data = await response.json();
-      return data; // should be an array of message objects
+      return data; 
     } catch (error) {
       console.error('Error fetching messages:', error);
       return [];
